cc04998a0e8dc0acc928e98372a25aceae204f7f
leey00nsu <dbstndla1212@naver.com>
2023-05-08 22:51:35 +0900

    #3 Detail Footer 컴포넌트 분리

--- src/components/ui/OutlineButton.tsx
+++ src/components/ui/OutlineButton.tsx
@@ -9,7 +9,7 @@ const OutlineButton = (props: OutlineButtonProps) => {
   return (
     <button
       onClick={props.onClick}
-      className="px-6 py-2 text-sm bg-white rounded-xl"
+      className="px-6 py-2 text-sm  rounded-xl"
       type="button"
     >
       {props.children}
--- src/components/ui/RoundedButton.tsx
+++ src/components/ui/RoundedButton.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 interface RoundedButtonProps {
+  color: string;
   size?: string;
   children: React.ReactNode;
 }
 const RoundedButton = (props: RoundedButtonProps) => {
-  return (
-    <button className="w-full py-2 text-sm font-medium text-white bg-af-hotPink font-Pretendard rounded-lg">
-      {props.children}
-    </button>
-  );
+  let classes = "";
+  if (props.color === "hotPink") {
+    classes =
+      "flex justify-center items-center w-full py-2 text-sm font-medium text-white bg-af-hotPink font-Pretendard rounded-lg h-full";
+  } else {
+    classes =
+      "flex justify-center items-center w-full py-2 text-sm font-medium text-white bg-af-dimGray font-Pretendard rounded-lg h-full";
+  }
+  return <button className={classes}>{props.children}</button>;
 };
 
 export default RoundedButton;
