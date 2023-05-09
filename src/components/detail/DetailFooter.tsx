ef37808741b72c256d64385420362d1187269ce0
leey00nsu <dbstndla1212@naver.com>
2023-05-09 14:01:54 +0900

    #7 커스텀 버튼을 daysiUI 버튼으로 대체

--- src/components/detail/DetailFooter.tsx
+++ src/components/detail/DetailFooter.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import OutlineButton from "../ui/OutlineButton";
-import RoundedButton from "../ui/RoundedButton";
+
 import { BsChevronCompactDown, BsChevronCompactUp } from "react-icons/bs";
 import { useState } from "react";
 import { motion } from "framer-motion";
@@ -23,9 +22,9 @@ const DetailFooter = (props: DetailFooterProps) => {
           transition={{ duration: 0.2 }}
           className="flex flex-col shrink-0 absolute bottom-0 z-50  items-center w-[400px] h-10 bg-af-brightGray rounded-t-3xl gap-2"
         >
-          <OutlineButton onClick={changeCollapsedHandler}>
+          <button onClick={changeCollapsedHandler}>
             <BsChevronCompactUp size={24} />
-          </OutlineButton>
+          </button>
         </motion.footer>
       ) : (
         <motion.footer
@@ -34,31 +33,18 @@ const DetailFooter = (props: DetailFooterProps) => {
           transition={{ duration: 0.2 }}
           className="flex flex-col shrink-0 absolute bottom-0 z-50  items-center w-[400px] h-40 bg-af-brightGray rounded-t-3xl gap-2"
         >
-          <OutlineButton onClick={changeCollapsedHandler}>
+          <button onClick={changeCollapsedHandler}>
             <BsChevronCompactDown size={24} />
-          </OutlineButton>
+          </button>
 
           <p className="text-xl font-semibold  text-af-hotPink">
             현재 n명이 작품 경매에 참여하고 있어요!
           </p>
 
-          {/* <div className="flex justify-evenly w-full ">
-      <div className="w-1/6 ">
-        <RoundedButton color="dimGray">
-          <BsArrowUp size={24} />
-        </RoundedButton>
-      </div>
-      <div className="w-1/2 ">
-        <RoundedButton color="dimGray">7000원</RoundedButton>
-      </div>
-      <div className="w-1/6 ">
-        <RoundedButton color="dimGray">
-          <BsArrowDown size={24} />
-        </RoundedButton>
-      </div>
-    </div> */}
-          <div className="flex w-2/4">
-            <RoundedButton color="hotPink">입찰하기</RoundedButton>
+          <div className="flex w-1/2 justify-center">
+            <button className="btn bg-af-hotPink hover:bg-af-hotPink w-full border-0">
+              입찰하기
+            </button>
           </div>
           {/* <BidButton onClick={publish}>100,000</BidButton> */}
         </motion.footer>
