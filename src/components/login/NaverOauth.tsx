b92e13aac8aaf8beedc0be3204bd74ce7a9519ca
ovovvvvv <ovovvvvv@naver.com>
2023-08-08 18:04:53 +0900

    #36 네이버 로그인 api테스트 완료

--- src/components/login/NaverOauth.tsx
+++ src/components/login/NaverOauth.tsx
@@ -4,8 +4,8 @@ import SocialButton from "./SocialButton";
 
 const NaverOauth = () => {
   const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
-  const REDIRECT_URI = `http://localhost:3000/NaverRedirect`;
-  const STATE_STRING = "false";
+  const REDIRECT_URI = encodeURI("http://localhost:3000/NaverRedirect");
+  const STATE_STRING = encodeURI("false");
   const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE_STRING}&redirect_uri=${REDIRECT_URI}`;
 
   const naverOAuthHandler = () => {
